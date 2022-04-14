33590a4d6d502672c76a73694e835140bf3523ed
Aristotia <antoine.belloni@gmail.com>
2022-04-14 11:58:49 +0200

    favourite card first render

--- frontend/src/components/DisplayFavouriteCard.jsx
+++ frontend/src/components/DisplayFavouriteCard.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import "./DisplayFavouriteCard.css";
 
 export default function DisplayFavouriteCard({ favouriteCard }) {
+  console.log(favouriteCard);
   return (
     <div className="CardStationComp">
       <div className="top-favourite-card">
         <h3>n°{favouriteCard[1].number}</h3>
-        <h2>{favouriteCard[1].name}</h2>
+        <h2>{favouriteCard[1].name.split(" - ").slice(1).join("-")}</h2>
+
         <img
-          src="../assets/Heart-favourite-card-full.png"
+          src="../src/assets/favourite-heart.png"
           alt="favourite-heart-full"
         />
       </div>
 
       <div className="middle-favourite-card">
-        <h2>{favouriteCard[1].adress}</h2>
+        <h2>{favouriteCard[1].address}</h2>
         {/* <h2>{favouriteCard.distance}</h2> */}
       </div>
 
       <div className="bottom-favourite-card">
-        <div id="available-bikes">
-          {favouriteCard[1].availableBikes}
-          <div id="available-bikes-stands">
-            {favouriteCard[1].availableBikesStands}
-          </div>
+        <div id="available-bikes">{favouriteCard[1].available_bikes}</div>
+        <div id="available-bike-stands">
+          {favouriteCard[1].available_bike_stands}
         </div>
       </div>
     </div>
