f427e71f2f8e00af19d0aeb03ddf8700b135bfd2
MidNightRoad <bergerpierre09@gmail.com>
2022-04-21 17:34:27 +0200

    fix conflict

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -48,7 +48,6 @@ export default function Map() {
         type="button"
         onClick={() => {
           setToggleSearch(!toggleSearch);
-          console.log("tacos");
         }}
       >
         la
