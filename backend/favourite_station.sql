d7da6078f44519b450b4eb726c76ae9a5d189201
francois_pobelle <francoispobelle@hotmail.fr>
2022-05-04 13:59:21 +0200

    desktop css

--- backend/favourite_station.sql
+++ backend/favourite_station.sql
@@ -1,49 +1,49 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- mysql dump 10.13  distrib 8.0.28, for win64 (x86_64)
 --
--- Host: localhost    Database: favourite_station
+-- host: localhost    database: favourite_station
 -- ------------------------------------------------------
--- Server version	8.0.28-0ubuntu0.20.04.3
+-- server version	8.0.28-0ubuntu0.20.04.3
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `favourite_station`
---
-
-DROP TABLE IF EXISTS `favourite_station`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `favourite_station` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 set @old_character_set_client=@@character_set_client */;
+/*!40101 set @old_character_set_results=@@character_set_results */;
+/*!40101 set @old_collation_connection=@@collation_connection */;
+/*!50503 set names utf8 */;
+/*!40103 set @old_time_zone=@@time_zone */;
+/*!40103 set time_zone='+00:00' */;
+/*!40014 set @old_unique_checks=@@unique_checks, unique_checks=0 */;
+/*!40014 set @old_foreign_key_checks=@@foreign_key_checks, foreign_key_checks=0 */;
+/*!40101 set @old_sql_mode=@@sql_mode, sql_mode='no_auto_value_on_zero' */;
+/*!40111 set @old_sql_notes=@@sql_notes, sql_notes=0 */;
 
 --
--- Dumping data for table `favourite_station`
+-- table structure for table `favourite_station`
 --
 
-LOCK TABLES `favourite_station` WRITE;
-/*!40000 ALTER TABLE `favourite_station` DISABLE KEYS */;
-/*!40000 ALTER TABLE `favourite_station` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+drop table if exists `favourite_station`;
+/*!40102 set @saved_cs_client     = @@character_set_client */;
+/*!50503 set character_set_client = utf8mb4 */;
+create table `favourite_station` (
+  `id` int not null auto_increment,
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
+/*!40101 set character_set_client = @saved_cs_client */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- dumping data for table `favourite_station`
+--
 
--- Dump completed on 2022-04-27 10:16:48
+lock tables `favourite_station` write;
+/*!40000 alter table `favourite_station` disable keys */;
+/*!40000 alter table `favourite_station` enable keys */;
+unlock tables;
+/*!40103 set time_zone=@old_time_zone */;
+
+/*!40101 set sql_mode=@old_sql_mode */;
+/*!40014 set foreign_key_checks=@old_foreign_key_checks */;
+/*!40014 set unique_checks=@old_unique_checks */;
+/*!40101 set character_set_client=@old_character_set_client */;
+/*!40101 set character_set_results=@old_character_set_results */;
+/*!40101 set collation_connection=@old_collation_connection */;
+/*!40111 set sql_notes=@old_sql_notes */;
+
+-- dump completed on 2022-04-27 10:16:48
--- frontend/src/components/CardStationDrop.jsx
+++ frontend/src/components/CardStationDrop.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/css/cardstationdrop.css";
+import axios from "axios";
 import JaugeVelo from "./JaugeVelo";
+import PhotoCard from "./PhotoCard";
 
 export default function CardStationDrop({ uniqueMarker, apiResult }) {
   const iteration = apiResult.indexOf(uniqueMarker);
   const nameStation = uniqueMarker.name.toLowerCase().substr(7).split("");
   const adressStation = uniqueMarker.address;
+  const [photoLink, setPhotoLink] = useState("");
+
+  const url = "https://api.pexels.com/v1/search?query=bicycle&per_page=79";
+  const config = {
+    headers: {
+      Authorization: "563492ad6f91700001000001fd0793323c9b47579668e757b6a46850",
+    },
+  };
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  useEffect(() => {
+    axios
+      .get(url, config)
+      // .then((response) => console.log(response))
+      .then((res) => {
+        setPhotoLink(res.data.photos[getRandomInt(78)].src.tiny);
+      });
+  }, []);
+
   return (
     <div className="leaflet-bottom cardstation">
       <div data-aos="fade-up" data-aos-duration="1000" className="card-station">
-        <div className="header-card">
-          <h2>{nameStation}</h2>
-          <img
-            src="../src/assets/empty-heart.png"
-            alt="heart"
-            className="heartimg"
-          />
-        </div>
-        <div className="card-paragraph">
-          <div className="jauge">
-            <JaugeVelo iteration={iteration} favouriteCard={apiResult} />
+        <div className="photo-city">
+          <PhotoCard photoSource={photoLink} />
+          <div className="name-jauge">
+            <div className="header-card">
+              <h2 className="name-station">{nameStation}</h2>
+              <img
+                src="../src/assets/empty-heart.png"
+                alt="heart"
+                className="heartimg"
+              />
+            </div>
+
+            <div className="card-paragraph">
+              <div className="jauge">
+                <JaugeVelo iteration={iteration} proximityStation={apiResult} />
+              </div>
+              <p className="adress-station">{adressStation}</p>
+            </div>
           </div>
         </div>
-        <p>{adressStation}</p>
       </div>
     </div>
   );
