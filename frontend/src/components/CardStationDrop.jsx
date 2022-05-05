d4024c24d3a1482afa3618fe25b89007a93b7ba0
francois_pobelle <francoispobelle@hotmail.fr>
2022-05-05 10:46:25 +0200

    fix conflict

--- frontend/src/components/CardStationDrop.jsx
+++ frontend/src/components/CardStationDrop.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "../assets/css/cardstationdrop.css";
 import axios from "axios";
@@ -53,7 +52,6 @@ export default function CardStationDrop({ uniqueMarker, apiResult }) {
   return (
     <div className="leaflet-bottom cardstation">
       <div data-aos="fade-up" data-aos-duration="1000" className="card-station">
-
         <div className="photo-city">
           <PhotoCard photoSource={photoLink} />
           <div className="name-jauge">
@@ -73,36 +71,37 @@ export default function CardStationDrop({ uniqueMarker, apiResult }) {
               <p className="adress-station">{adressStation}</p>
             </div>
 
-        <div className="header-card">
-          <h2>{nameStation}</h2>
-          {fav && (
-            <button
-              type="button"
-              className="fav-button"
-              onClick={handleClickFavourite}
-            >
-              {" "}
-              <img
-                src="../src/assets/favourite-heart.png"
-                alt="favourite-heart-full"
-              />
-            </button>
-          )}
-          {!fav && (
-            <button
-              type="button"
-              className="fav-button"
-              onClick={handleClickFavourite}
-            >
-              {" "}
-              <img src="../src/assets/empty-heart.png" alt="empty-heart" />
-            </button>
-          )}
-        </div>
-        <div className="card-paragraph">
-          <div className="jauge">
-            <JaugeVelo iteration={iteration} proximityStation={apiResult} />
-
+            <div className="header-card">
+              <h2>{nameStation}</h2>
+              {fav && (
+                <button
+                  type="button"
+                  className="fav-button"
+                  onClick={handleClickFavourite}
+                >
+                  {" "}
+                  <img
+                    src="../src/assets/favourite-heart.png"
+                    alt="favourite-heart-full"
+                  />
+                </button>
+              )}
+              {!fav && (
+                <button
+                  type="button"
+                  className="fav-button"
+                  onClick={handleClickFavourite}
+                >
+                  {" "}
+                  <img src="../src/assets/empty-heart.png" alt="empty-heart" />
+                </button>
+              )}
+            </div>
+            <div className="card-paragraph">
+              <div className="jauge">
+                <JaugeVelo iteration={iteration} proximityStation={apiResult} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
