f33ec4d0b887641b66b72053068407690be5a6fb
MidNightRoad <bergerpierre09@gmail.com>
2022-05-05 13:52:12 +0200

    add picture all team

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -20,7 +20,7 @@ export default function AboutUs() {
           <h1>About us </h1>
         </div>
         <h2>
-          Projet 2 pour la Wild Code School <br /> Fais avec &#10084;&#65039;
+          Projet 2 pour la Wild Code School <br /> Fais avec du &#10084;&#65039;
           pas mal de &#127866; et surtout des &#129369;
         </h2>
       </div>
@@ -28,7 +28,11 @@ export default function AboutUs() {
       <div className="card-container">
         <div className="card-member">
           <div className="image">
-            <img src="/src/assets/images/2ji8hx.jpg" alt="img" />
+            <img
+              src="/src/assets/images/antoine-aboutus.gif"
+              className="image-member"
+              alt="img"
+            />
           </div>
           <h2>Michel Tacos</h2>
           <h3>Les tacos c&#39;est la vie</h3>
@@ -50,7 +54,43 @@ export default function AboutUs() {
 
         <div className="card-member">
           <div className="image">
-            <img src="/src/assets/images/2ji8hx.jpg" alt="img" />
+            <img
+              src="/src/assets/images/pierre-aboutus.gif"
+              className="image-member"
+              alt="img"
+            />
+          </div>
+          <h2>Pierre Berger</h2>
+          <h3>
+            {" "}
+            <i>&quot;It&#39;s Birtney Bitch&quot;</i>
+          </h3>
+          <div className="member-icons">
+            <a href="https://github.com/Pierre-Berger">
+              <img
+                type="logo"
+                name="github"
+                src="/src/assets/images/github-logo-24.png"
+                alt="logo"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/pierre-berger-943903213/">
+              <img
+                name="linkedin"
+                type="logo"
+                src="/src/assets/images/linkedin-square-logo-24.png"
+                alt="logo"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="card-member">
+          <div className="image">
+            <img
+              src="/src/assets/images/francois-aboutus.gif"
+              className="image-member"
+              alt="img"
+            />
           </div>
           <h2>Michel Tacos</h2>
           <h3>Les tacos c&#39;est la vie</h3>
@@ -71,28 +111,11 @@ export default function AboutUs() {
         </div>
         <div className="card-member">
           <div className="image">
-            <img src="/src/assets/images/2ji8hx.jpg" alt="img" />
-          </div>
-          <h2>Michel Tacos</h2>
-          <h3>Les tacos c&#39;est la vie</h3>
-          <div className="member-icons">
             <img
-              type="logo"
-              name="github"
-              src="/src/assets/images/github-logo-24.png"
-              alt="logo"
+              src="/src/assets/images/breogan-aboutus.gif"
+              className="image-member"
+              alt="img"
             />
-            <img
-              name="linkedin"
-              type="logo"
-              src="/src/assets/images/linkedin-square-logo-24.png"
-              alt="logo"
-            />
-          </div>
-        </div>
-        <div className="card-member">
-          <div className="image">
-            <img src="/src/assets/images/2ji8hx.jpg" alt="img" />
           </div>
           <h2>Michel Tacos</h2>
           <h3>Les tacos c&#39;est la vie</h3>
