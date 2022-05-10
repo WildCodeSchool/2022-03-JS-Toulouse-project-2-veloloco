edea24522df415b98c30974d9d2d37096a3f8ae6
Aristotia <antoine.belloni@gmail.com>
2022-05-10 11:56:44 +0200

    link to game changed

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -154,7 +154,7 @@ export default function AboutUs() {
                 alt="logo"
               />
             </a>
-            <a href="https://flappyhenri.vercel.app/game.html">
+            <a href="https://flappyhenri.vercel.app/index.html">
               <img
                 name="logo-flappy-henri"
                 type="logo"
